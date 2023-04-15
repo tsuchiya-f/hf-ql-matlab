a31c523a32253a6c9679d2797bb2afd116d01b2e
tsuchiya-f <tsuchiya@pparc.gp.tohoku.ac.jp>
2023-04-15 18:16:38 +0900

    updated HF_dl_script.m

--- scripts/HF_dl_script.m
+++ scripts/HF_dl_script.m
@@ -1,65 +1,49 @@
-%------------------------------------------------------------------------------------
-% User inputs
-%------------------------------------------------------------------------------------
-%st_ctl_in.raw_ver1_corrected = 1;
-st_ctl_in.title = '';
+%
+% ex)   input_file = 'C:\share\Linux\doc\juice\ccsds\system_test\20210531_SCPFM_PTR_RPWI_2\20210531_SCPFM_PTR_RPWI_2_day3_xid32770.data.hf.ccsds'
+%       outdir = 'C:\share\Linux\doc\juice\ccsds\system_test\ql\'
+%       ret = HF_dl_script(input_file, outdir)
+%
 
-basedir_in  = "C:\share\Linux\juice_test\";
-basedir_out = "C:\share\Linux\juice_test\";
+function ret = HF_dl_script(input_file, outdir)
 
-% indir  = ["20211102_radio_burst_SGin\"];
-% outdir = ["20211102_radio_burst_SGin\"];
-% file_search_str = 'HF_20211102-1540.ccs';
+    %------------------------------------------------------------------------------------
+    % User inputs
+    %------------------------------------------------------------------------------------
+    st_ctl_in.raw_ver1_corrected = 1;
+    st_ctl_in.title = '';
+    %------------------------------------------------------------------------------------
+    
+    ql=0;
+    st_ctl_in.timeout=5;
+    
+    [indir_,infile_,inext_] = fileparts(input_file);
 
- indir  = ["20210927_Cgf6\"];
- outdir = ["20210927_Cgf6\DL\"];
- file_search_str = 'HF_20210927-1457.ccs';
+    indir = append(indir_, '\');
+    outext='.hf.ccsds';
 
-% indir  = [""];
-% outdir = [""];
-% file_search_str = 'HF_20211104-0939.ccs';
-
-%indir  = ["20211214_pssr3\"];
-%outdir = ["20211214_pssr3\"];
-%file_search_str = 'HF_*.ccs';
-
-%------------------------------------------------------------------------------------
-
-ql=0;
-st_ctl_in.timeout=5;
-
-n_dir = numel(indir);
-for j=1: n_dir
-
-    tdir = append(basedir_out, outdir(j)); 
-    mkdir(tdir)
-
-    s = dir(append(basedir_in, indir(j), file_search_str));
-    n_file = numel(s);
-
-    for i=1: n_file
-
-        file = s(i).name;
-
-        infile = append(basedir_in, indir(j), file);
-        outfile = append(basedir_out, outdir(j), file, '.hf.ccsds');
-        [ret, st_ctl] = hf_get_packet(infile, outfile);
-
-        if ret == 1 
-            fprintf("HF data in %s\n", file);
-%            fprintf("   SW ver : %d\n", st_ctl.ver);
-            fprintf("   number of HF packet      : %d\n",st_ctl.n_pkt);
-            fprintf("   number of HF data [Byte] : %d\n",st_ctl.out_sz);
-
-            st_ctl_in.dir_in = append(basedir_in, indir(j));
-            st_ctl_in.dir_out = append(basedir_out, outdir(j));
-            st_ctl_in.file_in = file;
-            hf_ccsds_ql(ql, st_ctl_in)
-
-        else
-            fprintf("No HF data in %s\n", file);
-        end
-
-        
+    if ~exist('outdir', 'var') 
+        outdir = indir_; 
     end
+    mkdir(outdir)
+    
+    infile = append(infile_, inext_);
+    output_file = append(outdir, infile, outext);
+    [ret_data, st_ctl] = hf_get_packet(input_file, output_file);
+    
+    if ret_data == 1 
+        fprintf("HF data in %s\n", infile);
+%        fprintf("   SW ver : %d\n", st_ctl.ver);
+        fprintf("   number of HF packet      : %d\n",st_ctl.n_pkt);
+        fprintf("   number of HF data [Byte] : %d\n",st_ctl.out_sz);
+    
+        st_ctl_in.dir_in  = indir;
+        st_ctl_in.dir_out = outdir;
+        st_ctl_in.file_in = infile;
+        hf_ccsds_ql(ql, st_ctl_in)
+    else
+        fprintf("No HF data in %s\n", infile);
+    end
+
+    ret = ret_data;
+
 end
