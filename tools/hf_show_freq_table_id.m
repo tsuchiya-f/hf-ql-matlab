af37eb4b22d6f719868036e13f25023fd0aa4108
Fuminori Tsuchiya <tsuchiya.f@tohoku.ac.jp>
2024-11-06 20:58:23 +0900

    update frequency table tool

--- tools/hf_show_freq_table_id.m
+++ tools/hf_show_freq_table_id.m
@@ -6,86 +6,96 @@ function f = hf_show_freq_table_id(sweep_table_id)
         % radio full
         ver = 3;
         sid = 3;
-        st_aux.sweep_table_id = 0x1f;
         st_hfa.n_band = 5;
         st_hfa.decimation   = 0;
         st_hfa.band0_startf = 191;
         st_hfa.band0_stopf  = 635;
         st_hfa.band0_step   = 2;
+        st_hfa.band0_rept   = 24;
         st_hfa.band0_sdiv   = 48;
         st_hfa.band1_startf = 635;
         st_hfa.band1_stopf  = 2189;
         st_hfa.band1_step   = 7;
+        st_hfa.band1_rept   = 8;
         st_hfa.band1_sdiv   = 12;
         st_hfa.band2_startf = 2189;
         st_hfa.band2_stopf  = 3743;
         st_hfa.band2_step   = 7;
+        st_hfa.band2_rept   = 2;
         st_hfa.band2_sdiv   = 3;
         st_hfa.band3_startf = 3743;
         st_hfa.band3_stopf  = 26387;
         st_hfa.band3_step   = 102;
+        st_hfa.band3_rept   = 1;
         st_hfa.band3_sdiv   = -3;
         st_hfa.band4_startf = 26387;
         st_hfa.band4_stopf  = 45035;
         st_hfa.band4_step   = 84;
+        st_hfa.band4_rept   = 1;
         st_hfa.band4_sdiv   = -4;
 
         case 1
         % radio full
         ver = 3;
         sid = 3;
-        st_aux.sweep_table_id = 0x1f;
         st_hfa.n_band = 5;
         st_hfa.decimation   = 0;
         st_hfa.band0_startf = 191;
         st_hfa.band0_stopf  = 635;
         st_hfa.band0_step   = 2;
+        st_hfa.band0_rept   = 32;
         st_hfa.band0_sdiv   = 48;
         st_hfa.band1_startf = 635;
         st_hfa.band1_stopf  = 2189;
         st_hfa.band1_step   = 7;
+        st_hfa.band1_rept   = 8;
         st_hfa.band1_sdiv   = 12;
         st_hfa.band2_startf = 2189;
         st_hfa.band2_stopf  = 3743;
         st_hfa.band2_step   = 7;
+        st_hfa.band2_rept   = 2;
         st_hfa.band2_sdiv   = 3;
         st_hfa.band3_startf = 3743;
         st_hfa.band3_stopf  = 16175;
         st_hfa.band3_step   = 56;
+        st_hfa.band3_rept   = 1;
         st_hfa.band3_sdiv   = -2;
         st_hfa.band4_startf = 16175;
         st_hfa.band4_stopf  = 40151;
         st_hfa.band4_step   = 108;
+        st_hfa.band4_rept   = 1;
         st_hfa.band4_sdiv   = -4;
 
         case 0
         % radio full
         ver = 2;
         sid = 3;
-        st_aux.sweep_table_id = 0x1F;
         st_hfa.n_band = 5;
         st_hfa.decimation   = 0;
         st_hfa.band0_startf = 191;
         st_hfa.band0_stopf  = 413;
         st_hfa.band0_step   = 1;
-        st_hfa.band0_rep    = 16;
+        st_hfa.band0_rept   = 16;
         st_hfa.band0_sdiv   = 24;
         st_hfa.band1_startf = 413;
         st_hfa.band1_stopf  = 635;
         st_hfa.band1_step   = 1;
-        st_hfa.band0_rep    = 16;
+        st_hfa.band1_rept   = 8;
         st_hfa.band1_sdiv   = 12;
         st_hfa.band2_startf = 635;
         st_hfa.band2_stopf  = 1079;
         st_hfa.band2_step   = 2;
+        st_hfa.band2_rept   = 4;
         st_hfa.band2_sdiv   = 6;
         st_hfa.band3_startf = 1079;
         st_hfa.band3_stopf  = 2189;
         st_hfa.band3_step   = 5;
+        st_hfa.band3_rept   = 2;
         st_hfa.band3_sdiv   = 3;
         st_hfa.band4_startf = 2189;
         st_hfa.band4_stopf  = 45035;
         st_hfa.band4_step   = 193;
+        st_hfa.band4_rept   = 1;
         st_hfa.band4_sdiv   = 1;
 
         otherwise
@@ -93,6 +103,8 @@ function f = hf_show_freq_table_id(sweep_table_id)
         fprintf('N.A. for Sweep table ID %d\n',sweep_table_id);
 
     end
+
+    st_aux.sweep_table_id = 0x1f;
 
     if ver ~= 0
 
